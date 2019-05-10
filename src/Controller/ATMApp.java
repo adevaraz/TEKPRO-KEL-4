6c14a541cf7e48c3d60b3d285bf0664dc0d4dad2
hafizmfadli <lshafizmf1st@gmail.com>
2019-05-10 09:40:10 +0700

    ijojioytjiohytihoythythyth
    hghgjhgjhgjhg

--- src/Controller/ATMApp.java
+++ src/Controller/ATMApp.java
@@ -5,5 +5,12 @@ public class ATMApp {
    public static void main(String[] args) {
       ATM theATM = new ATM();    
       theATM.run();
+      //asdsadasd
+      ///asdsadsadsad
+      ///dsfdsfdsfdsfdsfdsf
+      ///fdgfdgfdgdhhyrhyth
+      //sdfsdfsdfsdfdsf
+     // /dsfdsf/sdfsdfsdfsdf
+   //dfgdfgfdgfdgfdgdfgdffg
    }
 } 
